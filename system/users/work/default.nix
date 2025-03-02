84f0d7004549c5a77faab462e4601b947e2fed80
CrazyStevenz <github.ekta@aleeas.com>
2025-03-02 14:26:45 +0200

    Reduce differences with main repo

--- system/users/work/default.nix
+++ system/users/work/default.nix
@@ -4,7 +4,6 @@
   users.users.work.packages = with pkgs; [
     beekeeper-studio # SQL client
     google-cloud-sdk
-    # nodePackages.firebase-tools # Manage, and deploy your Firebase project from the command line
     slack # Desktop client for Slack
     watchman # Watches files and takes action when they change
   ];
