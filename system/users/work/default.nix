20aea6011339e7d0309e9d2d2e51e34b671d1043
CrazyStevenz <github.ekta@aleeas.com>
2025-03-08 20:27:18 +0200

    Remove unnecessary packages for work

--- system/users/work/default.nix
+++ system/users/work/default.nix
@@ -2,9 +2,6 @@
 
 {
   users.users.work.packages = with pkgs; [
-    beekeeper-studio # SQL client
-    google-cloud-sdk
-    # nodePackages.firebase-tools # Manage, and deploy your Firebase project from the command line
     slack # Desktop client for Slack
     watchman # Watches files and takes action when they change
   ];
