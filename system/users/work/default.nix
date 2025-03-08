ae6fe31e35780b8e423d22eeeb3ab86c3503eb71
CrazyStevenz <github.ekta@aleeas.com>
2025-03-08 20:05:17 +0200

    Remove google cloud sdk

--- system/users/work/default.nix
+++ system/users/work/default.nix
@@ -3,7 +3,6 @@
 {
   users.users.work.packages = with pkgs; [
     beekeeper-studio # SQL client
-    google-cloud-sdk
     slack # Desktop client for Slack
     watchman # Watches files and takes action when they change
   ];
