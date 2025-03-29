07fe88180be0ba5f6d95d0c594ffbbba4a0fb458
CrazyStevenz <github.ekta@aleeas.com>
2025-03-29 22:38:28 +0200

    Add postgres to work user

--- system/users/work/default.nix
+++ system/users/work/default.nix
@@ -7,4 +7,13 @@
     watchman # Watches files and takes action when they change
     windsurf # Agentic IDE powered by AI Flow paradigm
   ];
+
+  services.postgresql = {
+    enable = false;
+    package = pkgs.postgresql_16;
+    identMap = ''
+      # ArbitraryMapName systemUser DBUser
+        superuser_map    work       reader
+    '';
+  };
 }
