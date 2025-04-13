2db6e836a1202c9f6372b6349412906a36e323ee
CrazyStevenz <github.ekta@aleeas.com>
2025-04-13 13:02:59 +0300

    Disable beekeeper studio

--- system/users/work/default.nix
+++ system/users/work/default.nix
@@ -2,7 +2,7 @@
 
 {
   users.users.work.packages = with pkgs; [
-    beekeeper-studio # SQL client
+    # beekeeper-studio # SQL client
     slack # Desktop client for Slack
     watchman # Watches files and takes action when they change
     windsurf # Agentic IDE powered by AI Flow paradigm
