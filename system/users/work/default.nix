b604d74511e6b0de6f18edbfda804c48cab915d9
CrazyStevenz <github.ekta@aleeas.com>
2025-07-17 16:57:10 +0300

    Add cursor to work user

--- system/users/work/default.nix
+++ system/users/work/default.nix
@@ -3,6 +3,7 @@
 {
   users.users.work.packages = with pkgs; [
     # beekeeper-studio # SQL client
+    code-cursor # AI-powered code editor built on vscode
     slack # Desktop client for Slack
     watchman # Watches files and takes action when they change
     windsurf # Agentic IDE powered by AI Flow paradigm
