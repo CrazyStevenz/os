dfafd5d8b1ac8c43026f9662c3c80f25028b69eb
CrazyStevenz <github.ekta@aleeas.com>
2026-08-18 17:14:06 +0300

    Aadpt config

--- .state/flake.nix
+++ .state/flake.nix
@@ -9,7 +9,7 @@
       url = "github:nix-community/home-manager";
     };
     icedos-config = {
-      url = "path:/nix/store/ywi6d9f2w0l7gdbh52y7ldar6c4p9dm1-icedos-config";
+      url = "path:/nix/store/pa5difznpwjbiv8hn03dri2h8mv4xksz-icedos-config";
     };
     icedos-config-hytale-launcher-hytale-launcher = {
       inputs = {
@@ -22,39 +22,12 @@
     icedos-core = {
       follows = "icedos-config/icedos";
     };
-    icedos-github_icedborn_claude-icedos = {
-      url = "github:icedborn/claude-icedos/3872b8136f5c33748b9cc7fa8d6ef2a82a6bab75";
-    };
-    icedos-github_icedborn_claude-icedos-peon-ping-peon-ping = {
-      inputs = {
-        nixpkgs = {
-          follows = "nixpkgs";
-        };
-      };
-      url = "github:PeonPing/peon-ping";
-    };
     icedos-github_icedos_apps = {
-      url = "github:icedos/apps/0a59b448eb2de2ce9a2aa5b444d8836d5edfdaeb";
-    };
-    icedos-github_icedos_apps-aagl-aagl = {
-      inputs = {
-        nixpkgs = {
-          follows = "nixpkgs";
-        };
-      };
-      url = "github:ezKEa/aagl-gtk-on-nix";
+      url = "github:icedos/apps/c390dc21b80848e14c9bb96e246501d5fd823870";
     };
     icedos-github_icedos_apps-celluloid-celluloid-shader = {
       flake = false;
       url = "path:///nix/store/5zcj323fgw0vxx0nhgvp45yxrwikm0c6-FSR.glsl";
-    };
-    icedos-github_icedos_apps-proton-launch-scopebuddy = {
-      inputs = {
-        nixpkgs = {
-          follows = "nixpkgs";
-        };
-      };
-      url = "github:HikariKnight/ScopeBuddy";
     };
     icedos-github_icedos_desktop = {
       url = "github:icedos/desktop/c1a64443b11b20e479317ad3e324a1df14a781d4";
@@ -67,22 +40,11 @@
       };
       url = "github:nix-community/stylix";
     };
+    icedos-github_icedos_gnome = {
+      url = "github:icedos/gnome/936bf0d745c62bcc00777910d86b4a94ab7addb5";
+    };
     icedos-github_icedos_hardware = {
-      url = "github:icedos/hardware/47c378e42a2ae0666453b6d4868be8e093bea3af";
-    };
-    icedos-github_icedos_kde = {
-      url = "github:icedos/kde/90cf697215db44062300011e05f428df47fc4f71";
-    };
-    icedos-github_icedos_kde-default-plasma-manager = {
-      inputs = {
-        home-manager = {
-          follows = "home-manager";
-        };
-        nixpkgs = {
-          follows = "nixpkgs";
-        };
-      };
-      url = "github:nix-community/plasma-manager";
+      url = "github:icedos/hardware/4ea4e85b7df5d5b874cb2dbb34da228333a85cde";
     };
     icedos-github_icedos_providers = {
       url = "github:icedos/providers/c1a5aa2f9cdfd58f0c58ea78a4905c6afa9c373e";
@@ -95,7 +57,7 @@
     };
     icedos-state = {
       flake = false;
-      url = "path:/nix/store/p2gbyqprp3hcij13gkn1mqx2xf1iwkiy-icedos";
+      url = "path:/nix/store/xl58i7d670z75kmzlj81lf8sz20kazs1-icedos";
     };
     nixpkgs = {
       url = "github:nixos/nixpkgs/nixos-unstable";
@@ -155,7 +117,7 @@
             in
             {
               options.icedos.configurationLocation = mkStrOption {
-                default = "/home/stef/code/os/.state";
+                default = "/home/work/code/os/.state";
               };
             }
           )
@@ -176,7 +138,7 @@
                 (getModules "${inputs.icedos-config}/extra-modules")
               else
                 [ ];
-            config.system.stateVersion = "23.05";
+            config.system.stateVersion = "22.05";
           }
 
           home-manager.nixosModules.home-manager
@@ -218,7 +180,6 @@
                 "nvme"
                 "xhci_pci"
                 "ahci"
-                "usbhid"
                 "usb_storage"
                 "sd_mod"
               ];
@@ -227,32 +188,39 @@
               boot.extraModulePackages = [ ];
 
               fileSystems."/" = {
-                device = "/dev/disk/by-uuid/875ba1fd-ae85-47ec-beac-ec515e776834";
+                device = "/dev/disk/by-uuid/a96c8707-e60d-4a63-84e4-a09775df2bec";
                 fsType = "btrfs";
                 options = [ "subvol=@" ];
               };
 
-              boot.initrd.luks.devices."luks-a42d4af1-e764-4d91-acb2-ac735d979a64".device =
-                "/dev/disk/by-uuid/a42d4af1-e764-4d91-acb2-ac735d979a64";
+              boot.initrd.luks.devices."luks-1a3c2842-eb46-4f8f-9960-716acabc4b31".device =
+                "/dev/disk/by-uuid/1a3c2842-eb46-4f8f-9960-716acabc4b31";
+              boot.initrd.luks.devices."luks-a8cffca7-e268-4cb1-a7b8-2e14a3b56208".device =
+                "/dev/disk/by-uuid/a8cffca7-e268-4cb1-a7b8-2e14a3b56208";
 
-              fileSystems."/boot" = {
-                device = "/dev/disk/by-uuid/080E-B189";
+              fileSystems."/boot/efi" = {
+                device = "/dev/disk/by-uuid/7330-238A";
                 fsType = "vfat";
               };
 
-              swapDevices = [ ];
+              swapDevices = [
+                { device = "/dev/disk/by-uuid/edb966ac-b852-4051-bc93-668b968849a7"; }
+              ];
 
               # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
               # (the default) this is the recommended approach. When using systemd-networkd it's
               # still possible to use this option, but it's recommended to use it in conjunction
               # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
               networking.useDHCP = lib.mkDefault true;
-              # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
+              # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
+              # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
 
               nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
               hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
             }
           )
+
+          ({ })
 
         ]
         ++ modulesFromConfig.options
